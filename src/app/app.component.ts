0614af095b536b8516ffd7ddf0a6538780944e67
Luiz Damy <lgdamy@gmail.com>
2025-03-21 23:35:13 -0300

    Revert "fazendo o container ter posição fixa, posicao sticky no filtro"
    ficou uma merda em mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,13 @@
-import { AfterViewInit, Component, ElementRef, HostBinding, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
-import { ContainerService } from './common/container.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit,AfterViewInit {
+export class AppComponent {
   title = 'porto-guest-house';
-  @ViewChild('container') containerRef!: ElementRef;
 
-  constructor(
-    private readonly containerService: ContainerService,
-    private readonly router: Router,
-    ) {}
-
-  ngOnInit(): void {
-    this.router.events.subscribe(() => {
-      if (this.router.url == '/') {
-        this.containerRef.nativeElement.classList.add('menu-inicial');
-      } else {
-        this.containerRef.nativeElement.classList.remove('menu-inicial');
-      }
-    })
+  constructor() {
   }
-  ngAfterViewInit() {
-    if (this.containerRef) {
-      this.containerService.setContainer(this.containerRef);
-    }
-  }
-
 }
